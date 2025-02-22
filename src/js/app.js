f33608d8d56626703c3edede20bcb09e80ff821d
“krlo” <kirill.osadchenko@gmail.com>
2025-02-22 16:19:08 +0500

    correct solution

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,36 @@
-const fieldItems = Array.from(document.querySelectorAll(".field-item"));
+// const fieldItems = Array.from(document.querySelectorAll(".field-item"));
 
-function activateField() {
-  const activeIndex = Math.floor(1 + Math.random() * 15);
-  fieldItems[activeIndex].classList.add("field-item-active");
-}
+// function activateField() {
+//   const activeIndex = Math.floor(1 + Math.random() * 15);
+//   fieldItems[activeIndex].classList.add("field-item-active");
+// }
 
-function deactivateField() {
-  fieldItems.forEach((item) => {
-    if (item.classList.contains("field-item-active")) {
-      item.classList.remove("field-item-active");
-    }
-  });
-}
+// function deactivateField() {
+//   fieldItems.forEach((item) => {
+//     if (item.classList.contains("field-item-active")) {
+//       item.classList.remove("field-item-active");
+//     }
+//   });
+// }
 
-setInterval(() => {
-  deactivateField();
-  activateField();
-}, 1000);
+// setInterval(() => {
+//   deactivateField();
+//   activateField();
+// }, 1000);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fieldItems = Array.from(document.querySelectorAll(".field-item"));
+  const img = document.createElement("img");
+  img.src = "img/goblin.png";
+
+  function getItem() {
+    const startIndex = Math.floor(1 + Math.random() * fieldItems.length - 1);
+    return fieldItems[startIndex];
+  }
+
+  function activateField() {
+    const startItem = getItem();
+    startItem.append(img);
+  }
+  setInterval(activateField, 1000);
+});
