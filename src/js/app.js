ce41e5d4a368c6b43587d39ed9e4c3c7df718b9d
“krlo” <kirill.osadchenko@gmail.com>
2025-03-01 14:04:58 +0500

    hw correction

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,32 @@
+import image from "../img/goblin.png";
+
 document.addEventListener("DOMContentLoaded", () => {
+  const div = document.createElement("div");
+  div.className = "field-container";
+  document.body.append(div);
+
+  for (let i = 0; i < 16; i++) {
+    const item = document.createElement("div");
+    item.className = "field-item";
+    div.append(item);
+  }
+
   const fieldItems = Array.from(document.querySelectorAll(".field-item"));
   const img = document.createElement("img");
-  img.src = "img/goblin.png";
+  img.src = image;
 
   function getItem() {
-    const startIndex = Math.floor(1 + Math.random() * fieldItems.length - 1);
-    return fieldItems[startIndex];
+    let currentIndex = 0;
+    let generateIndex = Math.floor(1 + Math.random() * fieldItems.length - 1);
+    do {
+      currentIndex = generateIndex;
+      return fieldItems[currentIndex];
+    } while (currentIndex != generateIndex);
   }
 
   function activateField() {
-    const startItem = getItem();
-    startItem.append(img);
+    const activeItem = getItem();
+    activeItem.append(img);
   }
   setInterval(activateField, 1000);
 });
